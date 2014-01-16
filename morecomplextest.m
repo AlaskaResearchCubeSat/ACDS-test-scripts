5631b91d7ed839b6082dd5192da5675ebbe94f77
Jesse Frey <jmfrey@alaska.edu>
2014-01-16 10:52:10 -0900

    Fixed status parsing code functions

--- morecomplextest.m
+++ morecomplextest.m
@@ -304,25 +304,25 @@ function [xdat,ydat,zdat] = morecomplextest(com,baud)
 end
 
 function [stat]=stat_strip(line)
-    stsx=sscanf(line,'B\t%[+-] %*[+-] %*[+-] %*i %*i %*i');
-    stsy=sscanf(line,'B\t%*[+-] %[+-] %*[+-] %*i %*i %*i');
-    stsz=sscanf(line,'B\t%*[+-] %*[+-] %[+-] %*i %*i %*i');
+    stsx=sscanf(line,'%[+-] %*[+-] %*[+-] %*i %*i %*i');
+    stsy=sscanf(line,'%*[+-] %[+-] %*[+-] %*i %*i %*i');
+    stsz=sscanf(line,'%*[+-] %*[+-] %[+-] %*i %*i %*i');
     %reformat status
     stat=sprintf('%s  %s  %s',stsx,stsy,stsz);
 end
 
 function [dat]=stat_dat(line)
-    stsx=sscanf(line,'B\t%[+-] %*[+-] %*[+-] %*i %*i %*i');
-    stsy=sscanf(line,'B\t%*[+-] %[+-] %*[+-] %*i %*i %*i');
-    stsz=sscanf(line,'B\t%*[+-] %*[+-] %[+-] %*i %*i %*i');
+    stsx=sscanf(line,'%[+-] %*[+-] %*[+-] %*i %*i %*i');
+    stsy=sscanf(line,'%*[+-] %[+-] %*[+-] %*i %*i %*i');
+    stsz=sscanf(line,'%*[+-] %*[+-] %[+-] %*i %*i %*i');
     %reformat status
     dat=[stsx;stsy;stsz];
 end
 
 function [len]=stat_length(line)
-    lx=length(sscanf(line,'B\t%[+-] %*[+-] %*[+-] %*i %*i %*i'));
-    ly=length(sscanf(line,'B\t%*[+-] %[+-] %*[+-] %*i %*i %*i'));
-    lz=length(sscanf(line,'B\t%*[+-] %*[+-] %[+-] %*i %*i %*i'));
+    lx=length(sscanf(line,'%[+-] %*[+-] %*[+-] %*i %*i %*i'));
+    ly=length(sscanf(line,'%*[+-] %[+-] %*[+-] %*i %*i %*i'));
+    lz=length(sscanf(line,'%*[+-] %*[+-] %[+-] %*i %*i %*i'));
     %make sure lenghts are consistant
     if(lx~=ly || ly~=lz)
         error('Inconsistant status lengths %i %i %i',lx,ly,lz);
