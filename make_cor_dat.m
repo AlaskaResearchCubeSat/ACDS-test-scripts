919e52db947fabec88650db28178eabe7292c740
Jesse Frey <jmfrey@alaska.edu>
2014-06-11 14:36:47 -0800

    modified make_cor dat to send single precision floats

--- make_cor_dat.m
+++ make_cor_dat.m
@@ -8,19 +8,18 @@ function [dat]=make_cor_dat(cor,axis)
     if ~any(axis(2)==['+' '-'])
         error('Axis direction must be "+" or "-"');
     end
-    base=zeros(2,1);
     dat=zeros(512,1,'uint8');
-    base(1)=15-ceil(log2(max(max(abs(cor(1:2,:))))));
-    base(2)=15-ceil(log2(max(max(abs(cor(3:51,:))))));
-    base=cast(base,'uint8');
-    n=2.^double(base);
     dat(1:4)='COR ';
     dat(5:6)=axis;
-    dat(7:8)=base;
-    dat(9:12)=typecast(cast(round(cor(1:2,1)*n(1)),'int16'),'uint8');
-    dat(13:16)=typecast(cast(round(cor(1:2,2)*n(1)),'int16'),'uint8');
-    dat(17:114)=typecast(cast(round(cor(3:51,1)*n(2)),'int16'),'uint8');
-    dat(115:212)=typecast(cast(round(cor(3:51,2)*n(2)),'int16'),'uint8');
+    %correction scale values
+    dat(7:14)=typecast(cast(cor(1:2,1),'single'),'uint8');
+    dat(15:22)=typecast(cast(cor(1:2,2),'single'),'uint8');
+    %base offset
+    dat(23:26)=typecast(cast(cor(3,1),'single'),'uint8');
+    dat(27:30)=typecast(cast(cor(3,2),'single'),'uint8');
+    %offsets for each set of torquer states
+    dat(31:222)=typecast(cast(cor(4:51,1),'single'),'uint8');
+    dat(223:414)=typecast(cast(cor(4:51,2),'single'),'uint8');
     %calculate checksum for data
     check=mod(sum(dat(1:510)),2^16);
     dat(511:512)=typecast(cast(check,'uint16'),'uint8');
