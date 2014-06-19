bcd28b5e03be3209400d4964b910ff01c0a6aafd
Jesse Frey <jmfrey@alaska.edu>
2014-06-18 18:21:03 -0800

    Fixed store_all_cal to use serial object instead of serial port

--- store_all_cal.m
+++ store_all_cal.m
@@ -22,7 +22,7 @@ function store_all_cal(com,baud,gain,ADCgain)
 
         for k=1:length(axis_names)
             %calculate correction values
-            cor=calall(axis_names{k},com,baud,gain,ADCgain,a{k});
+            cor=calall(axis_names{k},ser,baud,gain,ADCgain,a{k});
             %TESTING: generate random data
             %cor=rand(51,2);
             %make data to send to ACDS
