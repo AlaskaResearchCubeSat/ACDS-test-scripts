87dd0f5a9c6052f8558d3d9ff059a98aaae63a03
Jesse Frey <jmfrey@alaska.edu>
2014-06-27 10:59:52 -0800

    Updates to store_all_cal it more or less works now

--- store_all_cal.m
+++ store_all_cal.m
@@ -1,9 +1,11 @@
-function store_all_cal(com,baud,gain,ADCgain)
+function cor=store_all_cal(com,baud,gain,ADCgain)
     axis_names={'X+','X-','Y+','Y-','Z+','Z-'};
     
+    store_axis={'Y-','Y+','Z+'};
+    
     a={[0 0 1;-1 0 0;0 1 0],[0 0 1;-1 0 0;0 -1 0],...           %X +/-
-       [1 0 0;0 0 1;0 -1 0],[1 0 0;0 0 1;0 1 0],...             %Y +/-
-       [1 0 0;0 1 0;0 0 1],[1 0 0;0 1 0;0 0 1],...             %Z +/-
+       [1 0 0;0 0 -1;0 -1 0],[-1 0  0;0 0 1;0 -1 0],...             %Y +/-
+       [0 -1 0;-1 0 0;0 0 1],[1 0 0;0 1 0;0 0 1],...             %Z +/-
        };
    
     try
@@ -11,6 +13,9 @@ function store_all_cal(com,baud,gain,ADCgain)
         oldpath=addpath('Z:\ADCS\functions','Z:\Software\Libraries\commands\Matlab','-end');
         %open serial port
         ser=serial(com,'BaudRate',baud);
+        %set input buffer size
+        set(ser,'InputBufferSize',2000)
+        set(ser,'OutputBufferSize',2000)
         %set timeout to 5s
         set(ser,'Timeout',5);        %open port
         fopen(ser);
@@ -19,20 +24,35 @@ function store_all_cal(com,baud,gain,ADCgain)
         %warnings from confusing Matlab if they are dumped to UART
         command(ser,'log error');
         waitReady(ser);
+        
+        cor=cell(1,length(store_axis));
 
-        for k=1:length(axis_names)
+        for k=1:length(store_axis)
+            %find axis in the list of axis names
+            idx=strcmp(store_axis{k},axis_names);
             %calculate correction values
-            cor=calall(axis_names{k},ser,baud,gain,ADCgain,a{k});
+            cor{k}=calall(axis_names{idx},ser,baud,gain,ADCgain,a{idx});
             %TESTING: generate random data
-            %cor=rand(51,2);
+            %cor{k}=rand(51,2);
+            %print out datasheet like values for comparison
+            mag_parm(reshape(cor{1}(1:3,[2,1]),1,[]),gain*ADCgain);
+            
+            asyncClose(ser);
             %make data to send to ACDS
-            dat=make_cor_dat(cor,axis_names{k});
-            %send data to ACDS
-            SPI_write(ser,'ACDS',89+k,dat);
+            dat=make_cor_dat(cor{k},axis_names{k});
+            try
+                %send data to ACDS
+                SPI_write(ser,'ACDS',89+k,dat); 
+            catch err
+                fprintf(2,'Error : sending data "%s"\n',err.message);
+            end
         end
         %connect to the ACDS board
         asyncOpen(ser,'ACDS');
-        for k=1:length(axis_names)
+        
+        fprintf('Unpacking correction data\n');
+        
+        for k=1:length(store_axis)
             command(ser,'unpack %i',89+k);
             %wait for command to finish
             waitReady(ser,[],true);
@@ -40,7 +60,7 @@ function store_all_cal(com,baud,gain,ADCgain)
         %close async
         asyncClose(ser);
     catch err
-        if exist('ser','var')
+        if exist('ser','var') && isvalid(ser)
             if strcmp(ser.Status,'open')
                 fclose(ser);
             end
