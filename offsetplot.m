740dc5522de6d554dd43a0d7fdaadec8a34a408c
Jesse Frey <jmfrey@alaska.edu>
2014-08-06 17:23:18 -0800

    Added lables and figure export to offsetplot

--- offsetplot.m
+++ offsetplot.m
@@ -5,15 +5,38 @@ function offsetplot(cor,boards)
     board_names={'X+','X-','Y+','Y-','Z+','Z-'};
     %tell which main axis each sensor axis corosponds to
     board_axes=[2,-3;  -2,-3;  -1,-3;  1,-3; 2,1; -1,2]; 
+    %index for status generation
+    idx=0:15;
+    %generate array for states
+    states=zeros(16,4);
+    %make binary matrix
+    for k=1:4
+        states(:,k)=~~bitand(idx,2^(k-1));
+    end
+    %generate char array of strings
+    states=char(ones(size(states))*'-'+('+'-'-')*states);
+    %index for states
+    Tidx=1:(3*16);
+    %default state
+    dstate='++--';
+    %empty cell array for lables
+    lables=cell(1,3*16);
+    %generate cell array for ticks
+    for k=Tidx
+        axis=ceil(k/16)-1;
+        lables{k}=[dstate,' ',dstate ' ',dstate];
+        lables{k}(5*axis+(1:4))=states(mod(k-1,16)+1,:);
+    end
     clf;
     fprintf('\n==========================[Correction Data Offsets]==========================\n');
+    
     for k=1:3
         %subplot for each axis
         subplot(3,1,k);
         hold on;
         cm=lines(5);
         cm_idx=1;
-        plots=false(size(board_names));
+        plots={};
         %print out axis maximum swing
         fprintf('%s-axis:\n',axes_names{k});
         for kk=1:length(boards)
@@ -26,20 +49,42 @@ function offsetplot(cor,boards)
                     offset=cor{kk}(4:end,jj);
                     offset=offset+cor{kk}(3,jj);
                     %plot error
-                    plot(offset,'Color',cm(cm_idx,:));
+                    plot(Tidx,offset,'Color',cm(cm_idx,:));
                     %get max and min
                     mx=max(offset)*1e3;
                     mn=min(offset)*1e3;
                     %print max and min
                     fprintf('\t%s SPB\n\t\tMax = %.2f mGauss\n\t\tMin =%.2f mGauss\n\t\tDeveation = %.2f mGauss\n',board_names{idx},mx,mn,mx-mn);
                     cm_idx=cm_idx+1;
-                    plots=plots | idx;
+                    plots={plots{:},board_names{idx}};
                 end
             end
         end
         hold off;
-        legend(board_names{plots});
-        xlabel('index');
+        legend(plots);
+        %set lables
+        set(gca,'XTick',Tidx);
+        if(k==3)
+            set(gca,'XTickLabel',lables);
+            xlabel('Torquer State');
+            %rotate tick lables so they are visable
+            %available:
+            %http://www.mathworks.com/matlabcentral/fileexchange/45172-rotatexlabels
+            rotateXLabels(gca,45);
+        else
+            set(gca,'XTickLabel',{});
+        end
     end
     fprintf('=============================================================================\n');
+    
+    %setup figure for printing
+    set(gcf,'PaperPositionMode','auto');
+    
+    %add functions folder to path
+    oldp=addpath('Z:\ADCS\functions');
+    %save plot
+    fig_export('Z:\ADCS\figures\board-offsets.eps');
+    %restore path
+    path(oldp);
+    
 end
