bbbcd71cae6ef538bceb30413c85eb0a15cee9ad
Jesse Frey <jmfrey@alaska.edu>
2014-10-07 09:44:50 -0800

    Updated alltorque test

--- alltorquetest.m
+++ alltorquetest.m
@@ -1,66 +1,77 @@
-
-ax_names={'X','Y','Z'};
-spb_names={'X+','X-','Y+','Y-','Z+','Z-'};
-
-test_names={'Y-','Y+','Z+'};
-
-a={ [0 0 1;-1 0 0;0 1 0],[0 0 1;-1 0 0;0 -1 0],...           %X +/-
-    [-1 0 0;0 0 1;0 1 0],[1 0  0;0 0 -1;0 1 0],...             %Y +/-
-    [0 1 0;1 0 0;0 0 -1],[1 0 0;0 1 0;0 0 1],...             %Z +/-
-    };
-
-%AM or PM strings for time display
-ampm={'AM','PM'};
-
-cstart=fix(clock);
-fprintf('Starting Test at %i:%02i:%02i\nSimulation Running Please Wait\n',cstart(4:6));
-
-%get start time to calculate elapsed time
-Tstart=tic();
-
-num=2*length(ax_names);
-
-for k=1:length(test_names)
-    for tq=1:4
-        for ax=1:length(ax_names)
-            %find SPB in the list of axis names
-            idx=strcmp(test_names{k},spb_names);
-            %array to set wich torquer to flip
-            torquer=zeros(1,3);
-            %set toruer
-            torquer(ax)=tq;
-            %[p,m]=offset_test(test_names{k},'COM6',9600,torquer,95.3,1);
-            [p,m]=offset_test(test_names{k},'COM3',57600,torquer,-95.3,1,a{idx});
-            %save figure
-            saveas(gcf(),['Z:\ADCS\figures\' test_names{k} ax_names{ax} int2str(tq) '-flip-fig'],'fig');
-            %save data
-            save(['Z:\ADCS\figures\' test_names{k} ax_names{ax} int2str(tq) '-flip-pm'],'p','m')
-            %===[estimate completeion time]===
-            %calculate iteration number
-            num=num+1;
-            %calculate done fraction
-            df=((tq-1)*length(ax_names)+ax)/num;
-            %get elapsed time
-            Te=toc(Tstart);%get remaining time
-            %calculate remaining time
-            Tr=Te*(df^-1-1);
-            %calculate completion estimate
-            tcomp=clock+[0 0 0 0 0 Tr];
-            %normalize completion estimate
-            tcomp=fix(datevec(datenum(tcomp)));
-            %get AM or PM
-            AMPM_idx=(tcomp(4)>12)+1;
-            if(AMPM_idx==2)
-                tcomp(4)=tcomp(4)-12;
-            end
-            %print new completion estimate
-            fprintf('Test %i of %i Complete\nCompletion estimate : %i/%i %i:%02i %s\n',(tq-1)*length(ax_names)+ax,2*length(ax_names),tcomp(2:5),ampm{AMPM_idx});
-        end
+function alltorquetest(test_names)
+    ax_names={'X','Y','Z'};
+    spb_names={'X+','X-','Y+','Y-','Z+','Z-'};
+    
+    if (~exist('test_names','var') || isempty(test_names))
+        test_names=spb_names;
     end
+
+    %rotation matricies for SPB's
+    a={make_spb_rot([0  -1 0],[0 0 -1]),make_spb_rot([0  1 0],[0 0 -1]),...           %X +/-
+       make_spb_rot([ 1 0 0],[0 0 -1]),make_spb_rot([-1 0 0],[0 0 -1]),...             %Y +/-
+       make_spb_rot([0  1 0],[-1  0 0]),make_spb_rot([-1 0 0],[0 1 0]),...             %Z +/-
+       };
+
+    %AM or PM strings for time display
+    ampm={'AM','PM'};
+
+    cstart=fix(clock);
+    fprintf('Starting Test at %i:%02i:%02i\nSimulation Running Please Wait\n',cstart(4:6));
+
+    %get start time to calculate elapsed time
+    Tstart=tic();
+
+    num=2*length(ax_names);
+    %create dat directory
+    quiet_mkdir(fullfile('.','dat'));
+    %generate new folder name for data
+    folder=unique_fliename(fullfile('.','dat','alltorque'));
+    %create folder
+    quiet_mkdir(folder);
+    %save SPB names and rotation matrix
+    save(fullfile(folder,'dat.mat'),'a','test_names','spb_names');
+
+    for k=1:length(test_names)
+        for tq=1:4
+            for ax=1:length(ax_names)
+                %find SPB in the list of axis names
+                idx=strcmp(test_names{k},spb_names);
+                %array to set wich torquer to flip
+                torquer=zeros(1,3);
+                %set toruer
+                torquer(ax)=tq;
+                [p,m]=offset_test(test_names{k},'COM3',57600,torquer,-95.3,1,a{idx});
+                %save figure
+                saveas(gcf(),fullfile(folder,[test_names{k},ax_names{ax},int2str(tq),'-flip-fig']),'fig');
+                %save data
+                save(fullfile(folder,[test_names{k},ax_names{ax},int2str(tq),'-flip-pm']),'p','m');
+                %===[estimate completeion time]===
+                %calculate iteration number
+                num=num+1;
+                %calculate done fraction
+                df=((tq-1)*length(ax_names)+ax)/num;
+                %get elapsed time
+                Te=toc(Tstart);%get remaining time
+                %calculate remaining time
+                Tr=Te*(df^-1-1);
+                %calculate completion estimate
+                tcomp=clock+[0 0 0 0 0 Tr];
+                %normalize completion estimate
+                tcomp=fix(datevec(datenum(tcomp)));
+                %get AM or PM
+                AMPM_idx=(tcomp(4)>12)+1;
+                if(AMPM_idx==2)
+                    tcomp(4)=tcomp(4)-12;
+                end
+                %print new completion estimate
+                fprintf('Test %i of %i Complete\nCompletion estimate : %i/%i %i:%02i %s\n',(tq-1)*length(ax_names)+ax,2*length(ax_names),tcomp(2:5),ampm{AMPM_idx});
+            end
+        end
+        tqOffsetPlot(test_names{k},folder);
+    end
+
+    %TODO: plot data
 end
 
-%plot data
-run('Z:\ADCS\figures\tqOffsetPlot');
 
 
-        
---
+++ tqOffsetPlot.m
@@ -0,0 +1,100 @@
+function tqOffsetPlot(SPB,folder)
+
+    %set a to quiet down Matlab
+    a=eye(3);
+    %load data
+    load(fullfile(folder,'dat.mat'));
+    %get base filename
+    basename=regexp(fullfile(folder), ['[^\' filesep ']+(?=\' filesep '?$)'], 'match');
+    %make basename not a cell array
+    basename=basename{:};
+    %make figures directory
+    quiet_mkdir(fullfile('.','figures'));
+
+    
+    %number of torquers in each axis
+    tq_axes=4;
+    %axis names
+    ax_names={'X' 'Y' 'Z'};
+    %file names for flip data
+    files=cell(length(ax_names)*tq_axes,1);
+    %lables for flip data
+    names=cell(length(ax_names)*tq_axes,1);
+    for k=1:length(ax_names)
+        for kk=1:tq_axes
+            idx=(k-1)*tq_axes+kk;
+            files{idx}=sprintf('%s%s%i-flip-pm.mat',SPB,ax_names{k},kk);
+            names{idx}=sprintf('%s%i',ax_names{k},kk);
+        end
+    end
+    
+    %get SPB board index
+    board_idx=strcmp(spb_names,'Y+');
+    
+    %get vector for which axis was measured
+    ax1=abs(a{board_idx}*[1;0;0])==1;
+    ax2=abs(a{board_idx}*[0;1;0])==1;
+    %get name of the measured axis
+    ax1_name=ax_names{ax1};
+    ax2_name=ax_names{ax2};
+    
+    %cell arrays for flip data
+    p=cell(size(files));
+    m=cell(size(files));
+    
+    v=zeros(length(files),2);
+    namesp=cell(size(files));
+    namesm=cell(size(files));
+
+    for k=1:length(files)
+        s=load(fullfile(folder,files{k}));
+        p{k}=s.p-ones(20,1)*median(s.p);
+        m{k}=s.m-ones(20,1)*median(s.m);
+        v(k,:)=abs(median(s.p(:,[3 6]))-median(s.m(:,[3 6])));
+        pdif=(max(s.p(:,[3 6]))-min(s.p(:,[3 6])))*1e3;
+        mdif=(max(s.m(:,[3 6]))-min(s.m(:,[3 6])))*1e3;
+        fprintf('%s error:\n\tX sensor : p = %f mGauss m = %f mGauss\n\tY sensor : p = %f mGauss m = %f mGauss\n',names{k},pdif(1),mdif(1),pdif(2),mdif(2));
+        namesp{k}=strcat(names{k},'+');
+        namesm{k}=strcat(names{k},'-');
+    end
+
+    %concatinate arrays
+    dat=vertcat(p{:},m{:});
+    %extract offsets
+    dat=dat(:,[3 6]);
+
+    idx=reshape(ones(length(dat)/length(names)/2,1)*(1:length(names)),[],1);
+    sn=sort([namesp namesm]);
+
+    %options for boxplot
+    boxoptions={'datalim',20e-3*[-1,1],'extrememode','compress'};
+
+    figure(1);
+    clf;
+
+    boxplot(dat(:,1),{namesp{idx},namesm{idx}},'grouporder',sn,boxoptions{:});
+    title(sprintf('%s SPB %s-axis',SPB,ax1_name));
+    ylabel('Field offset variation [Gauss]');
+
+    %save plot
+    fig_export(fullfile('.','figures',[basename,SPB,'-' ax1_name '.eps']));
+
+    figure(2);
+    clf;
+    boxplot(dat(:,2),{namesp{idx},namesm{idx}},'grouporder',sn,boxoptions{:});
+    title(sprintf('%s SPB %s-axis',SPB,ax2_name));
+    ylabel('Field offset variation [Gauss]');
+
+
+    %save plot
+    fig_export(fullfile('.','figures',[basename,SPB,'-' ax2_name '.eps']));
+
+    figure(3);
+    clf;
+    bar(v)
+    set(gca,'XtickLabel',names);
+    ylabel('Torquer Flip Offset [Gauss]');
+    %save plot
+    fig_export(fullfile('.','figures',[basename,SPB,'-values.eps']));
+
+end
