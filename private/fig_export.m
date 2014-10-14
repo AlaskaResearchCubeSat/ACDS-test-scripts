c09c4915bb85baf276ae9d4d030fb37a642a25fd
Jesse Frey <jmfrey@alaska.edu>
2014-10-14 09:30:11 -0800

    Fixed figure export to make nice figures now in PDF form

--- private/fig_export.m
+++ private/fig_export.m
@@ -2,6 +2,26 @@ function fig_export(h,file)
     if nargin<2
         %if only one argument given then treat it as the filename
         file=h;
+        %set h to current figure
+        h=gcf();
     end
-    print('-depsc2','-painters','-r200',file);
+    
+    %split filename
+    [path,basename,ext]=fileparts(file);
+    if(~strcmp(ext,'.pdf'))
+        ext='.pdf';
+        warning('Figure export type changed to PDF')
+    end
+    %recombine filename
+    file=fullfile(path,[basename,ext]);
+    %set to landscape
+    %set(h,'PaperOrientation','landscape');
+    %rotate page size so it looks correct
+    set(h,'PaperSize',fliplr(get(h,'PaperSize')));
+    %set to normalized units
+    set(h,'PaperUnits','normalized');
+    %set to fill page
+    set(h,'PaperPosition', [0 0 1 1]);
+    %generate print
+    print(h,'-painters','-dpdf','-r600',file);
 end
